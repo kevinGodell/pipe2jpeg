1dd025120596ebbf12237c9ab1f152a4a18f7cf4
Kevin Godell <kevin.godell@gmail.com>
2017-03-09 13:51:53 -0600

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
 # pipe2jpeg
-Parse individual jpegs from ffmpeg pipe. Used when ffmpeg output format is image2pipe, mjpeg, mpjeg, or singlejpeg and codec is mjpeg.
+Parse individual jpegs from an ffmpeg pipe when output codec is set to mjpeg and format is set to image2pipe, singlejpeg, mjpeg, or mpjeg.
+###installation:
+``` 
+npm install pipe2jpeg --save
+```
+###usage:
+```
+const spawn = require('child_process').spawn;
+
+const P2j = require('pipe2jpeg');
+
+const ffmpegParams = [
+    '-loglevel',
+    'quiet',
+    '-max_delay',
+    '0',
+    '-f',
+    'rtsp',
+    '-rtsp_transport',
+    'udp',
+    '-stimeout',
+    '10000000',
+    '-i',
+    'rtsp://192.168.1.8:554/user=admin_password=pass_channel=1_stream=0.sdp',
+    '-an',
+    '-c:v',
+    'mjpeg',//mjpeg is default codec for jpeg
+    '-f',
+    'image2pipe',//image2pipe, singlejpeg, mjpeg, or mpjpeg
+    '-r',
+    '5',
+    '-s',
+    '640x360',
+    'pipe:1'//output must be a pipe
+];
+
+const ffmpeg = spawn('ffmpeg', ffmpegParams);
+
+const p2j = new P2j().on('jpeg', (jpeg) => {
+    //will log size of jpeg and also show the SOI(0xFF, 0xD8) and EOI(0xFF, 0xD9) markers to verify 
+    console.log('found jpeg', jpeg.length, jpeg[0], jpeg[1], jpeg[jpeg.length -2], jpeg[jpeg.length]);
+});
+    
+ffmpeg.stdout.pipe(p2j);//target the pipe that was used in ffmpegParams, pipe:1 is stdout
---
+++ index.js
@@ -0,0 +1,50 @@
+// jshint esversion: 6, globalstrict: true, strict: true
+'use strict';
+
+const util = require('util');
+const Transform = require('stream').Transform;
+
+function Pipe2Jpeg() {
+    if (!(this instanceof Pipe2Jpeg)) {
+        return new Pipe2Jpeg();
+    }
+    Transform.call(this);
+    this._buffer = null;
+}
+
+util.inherits(Pipe2Jpeg, Transform);
+
+Pipe2Jpeg.prototype._transform = function (chunk, encoding, callback) {
+    var soi = null;
+    var eoi = null;
+    var jpeg = null;
+    for (let i = 0, length = chunk.length; i < length; i++) {
+        if (this._buffer || chunk[soi = i] === 0xFF && chunk[i + 1] === 0xD8) {
+            eoi = null;
+            for (i; i < length; i++) {
+                if (chunk[i] === 0xFF && chunk[i + 1] === 0xD9) {
+                    eoi = i + 2;
+                    if (this._buffer) {
+                        jpeg = Buffer.concat([this._buffer, chunk.slice(0, eoi)]);
+                        this._buffer = null;
+                    } else {
+                        jpeg = chunk.slice(soi, eoi);
+                    }
+                    this.emit('jpeg', jpeg);
+                    jpeg = null;
+                    break;
+                }
+            }
+            if (!eoi) {
+                if (this._buffer) {
+                    this._buffer = Buffer.concat([this._buffer, chunk]);
+                } else {
+                    this._buffer = chunk.slice(soi);
+                }
+            }
+        }
+    }
+    callback();
+};
+
+module.exports = Pipe2Jpeg;
