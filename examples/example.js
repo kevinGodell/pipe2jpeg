ce386e4956631394283da0a589060b927b34e7d0
Kevin Godell <kevin.godell@gmail.com>
2017-10-14 17:42:06 -0500

    updated example

---
+++ examples/example.js
@@ -0,0 +1,54 @@
+// jshint esversion: 6, globalstrict: true, strict: true
+'use strict';
+const P2J = require('../index');
+
+const spawn = require('child_process').spawn;
+
+let jpegCounter = 0;
+
+const params = [
+    /* log info to console */
+    '-loglevel',
+    'quiet',
+    
+    /* use an artificial video input */
+    '-re',
+    '-f',
+    'lavfi',
+    '-i',
+    'testsrc=size=1920x1080:rate=15',
+
+    /* set output flags */
+    '-an',
+    '-c:v',
+    'mjpeg',
+    '-pix_fmt',
+    'yuvj422p',
+    '-f',
+    'image2pipe',//image2pipe, singlejpeg, mjpeg, or mpjpeg
+    '-vf',
+    'fps=1,scale=640:360',
+    '-q',
+    '1',
+    '-frames',
+    '100',
+    'pipe:1'
+];
+
+const p2j = new P2J();
+
+p2j.on('jpeg', (jpeg) => {
+    console.log('received jpeg', ++jpegCounter);
+});
+
+const ffmpeg = spawn('ffmpeg', params, {stdio : ['ignore', 'pipe', 'ignore']});
+
+ffmpeg.on('error', (error) => {
+    console.log(error);
+});
+
+ffmpeg.on('exit', (code, signal) => {
+    console.log('exit', code, signal);
+});
+
+ffmpeg.stdout.pipe(p2j);
