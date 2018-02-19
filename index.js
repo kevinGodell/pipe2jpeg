d9051589b8e22fc14cf7b042fd92e85e79d92b12
Kevin Godell <kevin.godell@gmail.com>
2018-02-18 18:30:30 -0600

    converted to es6 class and added feature to get most recent parsed jpeg and its timestamp

--- index.js
+++ index.js
@@ -1,52 +1,59 @@
-// jshint esversion: 6, globalstrict: true, strict: true
 'use strict';
 
-const util = require('util');
 const Transform = require('stream').Transform;
 
-function Pipe2Jpeg() {
-    if (!(this instanceof Pipe2Jpeg)) {
-        return new Pipe2Jpeg();
+class Pipe2Jpeg extends Transform {
+
+    constructor(options) {
+        super(options);
+        this._buffer = null;
     }
-    Transform.call(this);
-    this._buffer = null;
-}
 
-util.inherits(Pipe2Jpeg, Transform);
+    get jpeg() {
+        return this._jpeg || null;
+    }
 
-Pipe2Jpeg.prototype._transform = function (chunk, encoding, callback) {
-    for (let i = 0, soi, eoi, jpeg, length = chunk.length; i < length; i++) {
-        if (this._buffer || chunk[soi = i] === 0xFF && chunk[i + 1] === 0xD8) {
-            eoi = null;
-            for (i; i < length; i++) {
-                if (chunk[i] === 0xFF && chunk[i + 1] === 0xD9) {
-                    eoi = i + 2;
-                    if (this._buffer) {
-                        jpeg = Buffer.concat([this._buffer, chunk.slice(0, eoi)]);
-                        this._buffer = null;
-                    } else {
-                        jpeg = chunk.slice(soi, eoi);
+    get timestamp() {
+        return this._timestamp || -1;
+    }
+
+    _transform(chunk, encoding, callback) {
+        for (let i = 0, soi, eoi, jpeg, length = chunk.length; i < length; i++) {
+            if (this._buffer || chunk[soi = i] === 0xFF && chunk[i + 1] === 0xD8) {
+                eoi = null;
+                for (i; i < length; i++) {
+                    if (chunk[i] === 0xFF && chunk[i + 1] === 0xD9) {
+                        eoi = i + 2;
+                        if (this._buffer) {
+                            jpeg = Buffer.concat([this._buffer, chunk.slice(0, eoi)]);
+                            this._buffer = null;
+                        } else {
+                            jpeg = chunk.slice(soi, eoi);
+                        }
+                        this._jpeg = jpeg;
+                        this._timestamp = Date.now();
+                        if (this._readableState.pipesCount > 0) {
+                            this.push(jpeg);
+                        }
+                        if (this.listenerCount('jpeg') > 0) {
+                            this.emit('jpeg', jpeg);
+                        }
+                        jpeg = null;
+                        break;
                     }
-                    if (this._readableState.pipesCount > 0) {
-                        this.push(jpeg);
-                    }
-                    if (this.listenerCount('jpeg') > 0) {
-                        this.emit('jpeg', jpeg);
-                    }
-                    jpeg = null;
-                    break;
                 }
-            }
-            if (!eoi) {
-                if (this._buffer) {
-                    this._buffer = Buffer.concat([this._buffer, chunk]);
-                } else {
-                    this._buffer = chunk.slice(soi);
+                if (!eoi) {
+                    if (this._buffer) {
+                        this._buffer = Buffer.concat([this._buffer, chunk]);
+                    } else {
+                        this._buffer = chunk.slice(soi);
+                    }
                 }
             }
         }
+        callback();
     }
-    callback();
-};
+
+}
 
 module.exports = Pipe2Jpeg;
