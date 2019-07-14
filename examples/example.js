c5800b341c38e6d54f09d0e6d2fe4fe701e4bf31
Kevin Godell <kevin.godell@gmail.com>
2019-07-14 14:10:43 -0500

    changed ffmpeg path

--- examples/example.js
+++ examples/example.js
@@ -1,54 +1,56 @@
-// jshint esversion: 6, globalstrict: true, strict: true
 'use strict';
-const P2J = require('../index');
+
+const P2J = require('..');
 
 const spawn = require('child_process').spawn;
+
+const ffmpegPath = require('@ffmpeg-installer/ffmpeg').path;
 
 let jpegCounter = 0;
 
 const params = [
-    /* log info to console */
-    '-loglevel',
-    'quiet',
-    
-    /* use an artificial video input */
-    '-re',
-    '-f',
-    'lavfi',
-    '-i',
-    'testsrc=size=1920x1080:rate=15',
+  /* log info to console */
+  '-loglevel',
+  'quiet',
 
-    /* set output flags */
-    '-an',
-    '-c:v',
-    'mjpeg',
-    '-pix_fmt',
-    'yuvj422p',
-    '-f',
-    'image2pipe',//image2pipe, singlejpeg, mjpeg, or mpjpeg
-    '-vf',
-    'fps=1,scale=640:360',
-    '-q',
-    '1',
-    '-frames',
-    '100',
-    'pipe:1'
+  /* use an artificial video input */
+  '-re',
+  '-f',
+  'lavfi',
+  '-i',
+  'testsrc=size=1920x1080:rate=15',
+
+  /* set output flags */
+  '-an',
+  '-c:v',
+  'mjpeg',
+  '-pix_fmt',
+  'yuvj422p',
+  '-f',
+  'image2pipe', //image2pipe, singlejpeg, mjpeg, or mpjpeg
+  '-vf',
+  'fps=1,scale=640:360',
+  '-q',
+  '1',
+  '-frames',
+  '100',
+  'pipe:1'
 ];
 
 const p2j = new P2J();
 
 p2j.on('jpeg', (jpeg) => {
-    console.log('received jpeg', ++jpegCounter);
+  console.log('received jpeg', ++jpegCounter, jpeg.length);
 });
 
-const ffmpeg = spawn('ffmpeg', params, {stdio : ['ignore', 'pipe', 'ignore']});
+const ffmpeg = spawn(ffmpegPath, params, { stdio: ['ignore', 'pipe', 'ignore'] });
 
 ffmpeg.on('error', (error) => {
-    console.log(error);
+  console.log(error);
 });
 
 ffmpeg.on('exit', (code, signal) => {
-    console.log('exit', code, signal);
+  console.log('exit', code, signal);
 });
 
 ffmpeg.stdout.pipe(p2j);
